3fdc05236ea57326f54cd7423eadaa93ce5fdedd
arnaudstiegler <arnaud.stiegler@hotmail.fr>
2018-11-16 12:51:02 -0500

    added mountain_car check

---
+++ agent_mountaincar.py
@@ -0,0 +1,139 @@
+
+from collections import deque
+import numpy as np
+import random
+from keras.layers import Convolution2D, Flatten, Dense, Activation, Conv2D
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.losses import binary_crossentropy, mean_squared_error
+from keras.initializers import random_normal
+
+
+class mcar_agent():
+    def __init__(self, state_space, action_space):
+
+        # Parameters of the environment
+        self.state_space = state_space
+        self.action_space = action_space
+
+        # Learning parameters
+        self.epsilon = 1.0
+        # Number of time steps over which the agent will explore
+        self.explore = 25000
+        # Final value for epsilon (once exploration is finished)
+        self.final_epsilon = 0.001
+
+        self.epsilon_decay = (self.final_epsilon - self.epsilon) / (self.explore)
+        self.gamma = 0.95
+
+        # Memory replay parameters
+        self.memory_size = 50000
+        # Format of an experience is: (state,previous_state,action,reward)
+        self.D = deque([], self.memory_size)
+
+        # Parameters for the MLP
+        self.learning_rate_cnn = 0.001
+        self.Q = self._build_model()
+
+        # Parameters for the ongoing episode
+        self.state = None
+        self.previous_state = None
+        self.action = None
+        self.reward = None
+        self.initial_move = True
+        self.observe_phase = True
+        self.observe_steps = 20000  # Number of steps for observation (no learning)
+
+
+        # Max number of steps between two experience replays
+        self.experience_nb_steps = 1  # We update at each step
+        # Size of a batch for experience replay
+        self.experience_batch_size = 32
+        # A counter of the number of steps since last experience replay
+        self.time_steps = 0
+
+    def reinitialize_agent(self):
+        # This function is actually useless
+        self.initial_move = True
+        self.time_steps = 0
+        self.D = deque([], self.memory_size)
+
+    def _build_model(self):
+
+        init = random_normal(mean=0.0, stddev=0.05, seed=None)
+        model = Sequential()
+        model.add(Dense(64, input_shape=(1,), activation='relu',kernel_initializer=init))
+        model.add(Dense(32, activation='relu',kernel_initializer=init))
+        model.add(Dense(self.action_space, activation='linear', kernel_initializer=init))
+        adam = Adam(lr=self.learning_rate_cnn)
+        model.compile(loss='mse', optimizer=adam)
+        return model
+
+    def experience_replay(self):
+        # if(self.batch_learning % self.experience_nb_steps==0 and self.batch_learning >= self.experience_batch_size):
+        if (self.time_steps > self.experience_batch_size):
+
+            batch = random.sample(self.D, self.experience_batch_size)
+
+            for i in range(0, len(batch)):
+                state_t = batch[i][1]
+                state_t1 = batch[i][0]
+                action = batch[i][2]
+                reward = batch[i][3]
+                done = batch[i][4]
+
+                '''
+                OLD VERSION
+
+                if(done):
+                    target=reward
+                else:
+                    #When predicting, predict returns [[proba1,proba2,proba3]]
+                    target = reward + self.gamma*np.amax(self.Q.predict(state_t1)[0])
+
+                # When predicting, predict returns [[proba1,proba2,proba3]]
+                target_f = self.Q.predict(state_t1)[0]
+                target_f[action] = target
+                target_f = np.array(target_f)
+                '''
+
+                target = reward
+                if not done:
+                    Q_next = self.Q.predict(state_t1)[0]
+                    target = (reward + self.gamma * np.amax(Q_next))
+
+                target_f = self.Q.predict(state_t)
+                target_f[0][action] = target
+
+                self.Q.fit(state_t, target_f.reshape((1, self.action_space)), epochs=1, verbose=0)
+
+            if (self.epsilon > self.final_epsilon):
+                self.epsilon += self.epsilon_decay
+
+    def act(self, state):
+        '''
+                Implement Epsilon-Greedy policy
+
+                Inputs:
+                value: numpy ndarray
+                        A vector of values of actions to choose from
+
+                Outputs:
+                action: int
+                        Index of the chosen action
+                '''
+
+        q_values = self.Q.predict(state)  # array of q(state,action) for all action
+
+        random_float = np.random.random()
+        if (random_float < self.epsilon):
+            new_action = np.random.randint(self.action_space)
+        else:
+            new_action = np.argmax(q_values)
+
+        # We have done an additional step
+        self.time_steps += 1
+        return new_action
+
+    def add_to_memory(self, state, previous_state, action, reward, done):
+        self.D.append([state, previous_state, action, reward, done])
---
+++ train_mcar.py
@@ -0,0 +1,130 @@
+import gym
+import numpy as np
+import agent_mountaincar
+import timeit
+from utils import process_obs
+import skimage
+
+env_to_use = 'MountainCar-v0'
+
+
+env = gym.make(env_to_use)
+
+'''
+
+env._max_episode_steps is set at 10000 which can lead to very long game.
+The issue with that is that the agent gets stuck in suboptimal minimas (namely not moving)
+To adress that, we will set it at 1000 
+
+'''
+
+
+
+#state_space = env.observation_space #Format: Box(250, 160, 3)
+#action_space = env.action_space #Format: Discrete(3)
+
+state_space = 2
+action_space = 3
+
+
+'''
+
+env.step() -> returns array (state,reward,done?,_info)
+
+Action State for Time pilot
+action=1 -> going straight
+action=2 -> going up no fire
+action=3 -> going right no fire
+action=4 -> going left no fire
+'''
+
+#We initialize our agent
+
+agent = agent_mountaincar.mcar_agent(state_space= state_space, action_space= action_space)
+
+reward_list = []
+eps_length_list = []
+
+
+#TODO: render into agent class
+#TODO: Check all parameters for Network/Learning
+#TODO: Check reward
+#TODO: Check TD target
+#TODO: add epochs count
+#TODO: add performance testing after each epoch
+
+ep = 0
+
+while(True):
+
+    print("---- Currently running episode " +str(ep))
+    start = timeit.default_timer()
+
+    total_reward = 0
+    steps_in_ep = 0
+
+    #agent.reinitialize_agent()
+
+    # Initial state
+    s_t = env.reset() #Observation is array (250, 160, 3)
+
+    #s_t = s_t.reshape((2,1))
+    print(s_t)
+    #state = process_obs(obs)#to create a batch with only one observation
+    done=False
+
+    #Max number of rounds for one episode
+    while(done is False):
+
+        if(agent.initial_move):
+            # If it is the first move, we can't store anything in the memory
+            action = agent.act(s_t)
+            s_t1, reward, done, _info = env.step(action)
+            agent.state = s_t1
+            agent.initial_move = False
+
+        elif(agent.observe_phase):
+            #While we observe, we do not want to do replay_memory
+            # take step
+            action = agent.act(s_t)
+            s_t1, reward, done, _info = env.step(action)
+            agent.state = s_t1
+            agent.add_to_memory(agent.state, agent.previous_state, action, reward, done)
+            if(agent.time_steps > agent.observe_steps):
+                agent.observe_phase = False
+
+        else:
+            # take step
+            action = agent.act(s_t)
+            s_t1,reward,done,_info = env.step(action)
+            agent.state = s_t1
+            agent.add_to_memory(agent.state,agent.previous_state,action,reward,done)
+            agent.experience_replay()
+
+
+
+
+        #env.render()
+
+        total_reward += reward
+        steps_in_ep += 1
+        s_t = s_t1
+        agent.previous_state = s_t1
+
+    print("total reward: " + str(total_reward))
+    print("total number of steps: " + str(steps_in_ep))
+    print("agent epsilon: " + str(agent.epsilon))
+    reward_list.append(total_reward)
+    eps_length_list.append(steps_in_ep)
+
+    #We backup the weights
+    agent.Q.save_weights('/moutain_car/dqn.h5')
+    #We backup the rewards
+    np.savetxt("/mountain_car/rewards", reward_list)
+    np.savetxt("/mountain_car/steps", eps_length_list)
+
+    end = timeit.default_timer()
+    print("Episode took " + str((end-start)) + " seconds")
+
+agent.Q.save_weights('/moutain_car/dqn.h5')
+
