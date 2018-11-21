0862134542b792f5bc18c00f3c4bcf1fbc257a15
arnaudstiegler <arnaud.stiegler@hotmail.fr>
2018-11-21 18:38:21 -0500

    downgrade nn

--- DQL.py
+++ DQL.py
@@ -80,15 +80,13 @@ class DQL_agent():
         model.compile(loss='mse', optimizer=adam)
         '''
         model = Sequential()
-        model.add(Convolution2D(32, 8, 8, subsample=(4, 4), border_mode='same',
+        model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode='same',
                                 input_shape=(img_rows, img_cols,img_channels)))  # 80*80*4
         model.add(Activation('relu'))
-        model.add(Convolution2D(64, 4, 4, subsample=(2, 2), border_mode='same'))
-        model.add(Activation('relu'))
-        model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='same'))
+        model.add(Convolution2D(32, 4, 4, subsample=(2, 2), border_mode='same'))
         model.add(Activation('relu'))
         model.add(Flatten())
-        model.add(Dense(512))
+        model.add(Dense(256))
         model.add(Activation('relu'))
         model.add(Dense(self.action_space))
 
