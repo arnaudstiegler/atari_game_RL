f9510f159146fefd519452cca8c55eff4082d985
arnaudstiegler <arnaud.stiegler@hotmail.fr>
2018-12-14 12:39:16 -0500

    hyperparameter tuning

--- img_breakout/train_agent.py
+++ img_breakout/train_agent.py
@@ -64,9 +64,9 @@ while(True):
 
         #FOR DDQN
         #Pycharm refers to the base DQL model but when running it from the console, it uses /ram_breakout/DQL
-        #if(agent.time_steps % agent.update_target_Q == 0 and agent.use_target):
-            #print("update target network")
-            #agent.target_Q.set_weights(agent.Q.get_weights())
+        if(agent.time_steps % agent.update_target_Q == 0 and agent.use_target):
+            print("update target network")
+            agent.target_Q.set_weights(agent.Q.get_weights())
 
         action = agent.act(s_t)
         new_state,reward,done,_info = env.step(action)
